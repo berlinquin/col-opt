af2bf4514c6aef106916c1ca46063b711532e01b
Quinten Kent <berlinquin@gmail.com>
2021-11-26 16:57:52 -0600

    Call parse_csv with first arg passed to program

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
-int main(int argc, char **argv)
+#include "parser.h"
+#include "Matrix.h"
+
+int main(int argc, char *argv[])
 {
-   std::cout << "Hello world" << std::endl;
+   if (argc < 2)
+   {
+      std::cout << "ERROR: Must pass filename as an argument" << std::endl;
+      return 1;
+   }
+
+   // If argv has a filename, open that for reading
+   // Otherwise, read from stdin
+   Matrix *cell_lengths;
+   bool success = parse_csv(argv[1], &cell_lengths);
    return 0;
 }
