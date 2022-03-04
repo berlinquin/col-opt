a99dfd008e1216350dab88e5d755e61b0642281f
Quinten Kent <berlinquin@gmail.com>
2022-03-04 15:56:31 -0600

    The element size in cellLengths needs to match the emscripten HEAP view used

--- public_html/hello3.cpp
+++ public_html/hello3.cpp
@@ -21,29 +21,32 @@ EMSCRIPTEN_KEEPALIVE void process_table(uint16_t *table, int rowsParsed, int col
 			"table: %p, rowsParsed: %d, columns: %d\n",
 			table, rowsParsed, columns);
 	// Print as 2D array
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < rowsParsed; i++)
+	for (int i = 0; i < rowsParsed; i++)
 	{
 		printf("row %d: [", i);
-		for (j = 0; j < columns; j++)
+		for (int j = 0; j < columns; j++)
 		{
 			int index = i*columns + j;
-			printf("%hd, ", table[index]);
+			uint16_t x = table[index];
+			printf("%hd, ", x);
 		}
 		printf("]\n");
 	}
-	printf("last address: %p\n", &(table[i*columns + j]));
-	// Print as flat array
-	uint16_t *table_flat = reinterpret_cast<uint16_t*>(table);
-	int length = rowsParsed * columns;
-	printf("[");
-	for (i = 0; i < length; i++)
+	// Print individual bytes
+	/*
+	printf("print individual bytes\n");
+	int bytes = rowsParsed * columns * 2;
+	uint8_t *table_bytes = (uint8_t*) table;
+	for (int i = 0; i < bytes; i++)
 	{
-		printf("%hd, ", table_flat[i]);
+		printf("%p: %x\n", (table_bytes+i), table_bytes[i]);
+		if (i > 0 && i % (columns*2) == (columns*2)-1)
+		{
+			printf("\n");
+		}
 	}
-	printf("]\n");
-	printf("last address: %p\n", &(table_flat[i]));
+	printf("\n");
+	*/
 }
 
 #ifdef __cplusplus
