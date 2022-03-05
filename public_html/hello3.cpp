4a103ca7e3726656750d43bed731fd809c73774c
Quinten Kent <berlinquin@gmail.com>
2022-03-05 11:07:41 -0600

    Remove manual pointer arithmetic

--- public_html/hello3.cpp
+++ public_html/hello3.cpp
@@ -20,19 +20,6 @@ EMSCRIPTEN_KEEPALIVE void process_table(uint16_t *table, int rowsParsed, int col
 	printf("process_table() called with args:\n"
 			"table: %p, rowsParsed: %d, columns: %d\n",
 			table, rowsParsed, columns);
-	// Print as 2D array
-	printf("Manual pointer arithmetic:\n");
-	for (int i = 0; i < rowsParsed; i++)
-	{
-		printf("row %d: [", i);
-		for (int j = 0; j < columns; j++)
-		{
-			int index = i*columns + j;
-			uint16_t x = table[index];
-			printf("%hd, ", x);
-		}
-		printf("]\n");
-	}
 	// Print as boost multiarray
 	printf("Boost multiarray:\n");
 	boost::multi_array_ref<uint16_t, 2> boost_table(table, boost::extents[rowsParsed][columns]);
@@ -46,21 +33,6 @@ EMSCRIPTEN_KEEPALIVE void process_table(uint16_t *table, int rowsParsed, int col
 		}
 		printf("]\n");
 	}
-	// Print individual bytes
-	/*
-	printf("print individual bytes\n");
-	int bytes = rowsParsed * columns * 2;
-	uint8_t *table_bytes = (uint8_t*) table;
-	for (int i = 0; i < bytes; i++)
-	{
-		printf("%p: %x\n", (table_bytes+i), table_bytes[i]);
-		if (i > 0 && i % (columns*2) == (columns*2)-1)
-		{
-			printf("\n");
-		}
-	}
-	printf("\n");
-	*/
 }
 
 #ifdef __cplusplus
